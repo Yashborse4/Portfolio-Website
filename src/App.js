818402119d89f2e03e20119b4ba2265bc991fa2c
Portfolio Developer <developer@portfolio.local>
2025-08-10 12:16:06 +0530

    Initial commit: React portfolio app with sample files removed

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Portfolio</h1>
+        <p>
+          Welcome to my portfolio website.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
