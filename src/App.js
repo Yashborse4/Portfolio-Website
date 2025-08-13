94d23dbb9f9cd84e4ec6914b3e161ac1d129a2c2
Portfolio Developer <developer@portfolio.local>
2025-08-13 22:49:28 +0530

    \Fix React Router 404 issue by switching to HashRouter for GitHub Pages compatibility"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider as StyledThemeProvider } from 'styled-components';
 import { ThemeProvider, useTheme } from './context/ThemeContext';
 import GlobalStyles from './styles/GlobalStyles';
